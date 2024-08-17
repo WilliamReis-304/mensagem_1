ffcf620e458b5293c6ffab455668b897b320a79e
unknown <williamsil304@gmail.com>
2024-08-17 13:31:16 -0300

    correção de problemas com as mensagens sxibidas

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-const botaoAjudar = document.getElementById('ajudar')
 const botaoRecusar = document.getElementById('recusar')
 
-botaoAjudar.addEventListener("click",()=>{
-    let div = document.createElement('div')
-    div.innerHTML = `<p color='green'>https://drive.google.com/drive/folders/1iLGgk6HZcv4HZJl-7wMNTPSNZ4HgYYcO?usp=sharing</p>`
-    document.body.appendChild(div)
-})
 botaoRecusar.addEventListener("click",()=>{
     let div = document.createElement('div')
     div.innerHTML = `<p color='green'>Aproveite a vida enquanto puder!</p>`
